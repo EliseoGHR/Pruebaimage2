e17fd4caf09fac11b6203ec7d9c14d3a8049b9d2
Eliseo Serrano <sg24002@outlook.com>
2024-02-20 19:13:42 -0600

    Agregamos DbContext

---
+++ Pruebaimage2/Models/Pruebaimage2DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pruebaimage2.Models
+{
+    public class Pruebaimage2DbContext:DbContext
+    {
+        public Pruebaimage2DbContext(DbContextOptions<Pruebaimage2DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Carro> Carros { get; set; }
+
+    }
+
+}
+}
