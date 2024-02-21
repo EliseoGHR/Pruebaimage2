45349c7a88a0f162ddecb05ab7d821a524691bed
Eliseo Serrano <sg24002@outlook.com>
2024-02-20 19:50:26 -0600

    Hacemos migracion, agregamos controladores y configuramos el layout

---
+++ Pruebaimage2/Migrations/20240221014512_IntialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pruebaimage2.Migrations
+{
+    public partial class IntialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    MarcaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.MarcaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    CarroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Imagen = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    MarcaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.CarroId);
+                    table.ForeignKey(
+                        name: "FK_Carros_Marcas_MarcaId",
+                        column: x => x.MarcaId,
+                        principalTable: "Marcas",
+                        principalColumn: "MarcaId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carros_MarcaId",
+                table: "Carros",
+                column: "MarcaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carros");
+
+            migrationBuilder.DropTable(
+                name: "Marcas");
+        }
+    }
+}
