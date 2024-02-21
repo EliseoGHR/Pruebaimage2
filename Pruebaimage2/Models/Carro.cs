7dee6070444082f7ea87d040431fa29a920b2c26
Eliseo Serrano <sg24002@outlook.com>
2024-02-20 18:25:38 -0600

    Creamos dos clases del models

---
+++ Pruebaimage2/Models/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pruebaimage2.Models
+{
+    public class Carro
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CarroId { get; set; }
+
+        [Required(ErrorMessage = "El modelo del carro es obligatorio.")]
+        public string Modelo { get; set; }
+
+        // Propiedad de descripción del carro, nullable
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "El precio del carro es obligatorio.")]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Precio { get; set; }
+        // Propiedad de la imagen del carro
+        public byte[]? Imagen { get; set; }
+
+        // Propiedad de navegación para la relación con Marca
+        public int? MarcaId { get; set; }
+        [ForeignKey("MarcaId")]
+        public virtual Marca? Marca { get; set; }
+
+    }
+}
---
+++ Pruebaimage2/Models/Marca.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pruebaimage2.Models
+{
+    public class Marca
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MarcaId { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre de la marca es obligatorio.")]
+        public string Nombre { get; set; }
+
+        // Relación uno a muchos con Carro
+        public virtual ICollection<Carro> Carros { get; set; }
+    }
+}
