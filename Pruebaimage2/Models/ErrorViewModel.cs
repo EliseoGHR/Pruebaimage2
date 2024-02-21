01d7f3c549bb5781b26eb6fb52ce1afd3848c9c1
Eliseo Serrano <sg24002@outlook.com>
2024-02-20 18:00:48 -0600

    Agregar archivos de proyecto.

---
+++ Pruebaimage2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pruebaimage2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
